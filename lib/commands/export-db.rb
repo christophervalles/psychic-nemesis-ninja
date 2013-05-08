0915cba6c11c25ee409c5acdc85c13f4e8300e0c
Christopher Valles <christopher.valles@gree.net>
2013-05-08 18:23:24 +0100

    Adding export/import database commands

--- lib/commands/export-db.rb
+++ lib/commands/export-db.rb
@@ -2,7 +2,7 @@ class ExportDb < Vagrant.plugin(2, :command)
   def execute
     with_target_vms([], :single_target => true) do |vm|
       @logger.debug("Exporting databases to vagrant folder")
-      env = vm.action(:ssh_run, :ssh_run_command => "mysqldump -uroot --all-databases > /vagrant/exported_dbs.sql")
+      env = vm.action(:ssh_run, :ssh_run_command => "filename=`date +%Y%m%d-%H%M%S`; mkdir -p /vagrant/db-export;  mysqldump -uroot --all-databases > /vagrant/db-export/${filename}.sql")
       
       # Exit with the exit status of the command or a 0 if we didn't
       # get one.
--- lib/commands/import-db.rb
+++ lib/commands/import-db.rb
@@ -2,7 +2,7 @@ class ImportDb < Vagrant.plugin(2, :command)
   def execute
     with_target_vms([], :single_target => true) do |vm|
       @logger.debug("Import databases from /vagrant/exported_dbs.sql")
-      env = vm.action(:ssh_run, :ssh_run_command => "mysql -uroot < /vagrant/exported_dbs.sql")
+      env = vm.action(:ssh_run, :ssh_run_command => "filename=`ls | sort -r | head -1`; mysql -uroot < /vagrant/db-export/${filename}")
       
       # Exit with the exit status of the command or a 0 if we didn't
       # get one.
